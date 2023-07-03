43dc7fb29ea5b43ca45153e2e55d591c775ef8d8
maejor101 <vusumuzitswai@gmail.com>
2023-07-03 08:22:03 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,89 @@
+
+// POP UP MODAL 
+var modal = document.getElementById("myModal");   // Get the modal
+var btn = document.getElementById("myBtn"); // Get the button that opens the modal
+var span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
+
+// CART POP UP MODAL
+
+var cartMod = document.getElementById("cartModal");   // Get the modal
+var cartButton = document.getElementById("cartBtn"); // Get the button that opens the modal
+var cartSpan = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
+
+cartButton.onclick = function () {              // When the user clicks on the button, open the modal
+  cartMod.style.display = "block";
+}
+
+
+cartSpan.onclick = function () {
+  cartMod.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == cartMod) {
+    cartMod.style.display = "none";
+  }
+}
+
+
+
+btn.onclick = function () {              // When the user clicks on the button, open the modal
+  modal.style.display = "block";
+}
+span.onclick = function () {         // When the user clicks on <span> (x), close the modal
+  modal.style.display = "none";
+}
+window.onclick = function (event) {      // When the user clicks anywhere outside of the modal, close it
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//HTML  Page navigation
+
+function btn_menu() {
+  windows.location.href = "menu.html";
+}
+
+function btn_order() {
+  windows.location.href = "orders.html";
+}
+
+function nav_menu() {
+  windows.location.href = "menu.html";
+}
+function nav_home() {
+  windows.location.href = "home.html";
+}
+function nav_order() {
+  windows.location.href = "orders.html";
+}
+
+
+
+// STORING ORDERED ITEMS 
+
+function storeOrder() {
+  let arr = [];
+  let checkboxes = document.querySelectorAll("input[type='checkbox']:checked");
+  for (let i = 0; i < checkboxes.length; i++) {
+    arr.push(checkboxes[i].value)
+
+  }
+
+  // Converted array to string & alert
+  alert(arr.toString());
+
+}
+
+function storeRadio() {
+  let rad = [];
+  let radBtn = document.querySelectorAll("input[type='radio']:checked");
+  for (let i = 0; i < radBtn.length; i++) {
+    rad.push(radBtn[i].value)
+  }
+
+  alert(rad.toString());
+}
+
+
